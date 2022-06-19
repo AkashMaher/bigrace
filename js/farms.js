2c856051d80d02f6fafa4a8ca322a27aae16b594
Akash <aakashum121@gmail.com>
2022-06-19 23:35:09 +0530

    fix

--- js/farms.js
+++ js/farms.js
@@ -176,8 +176,8 @@ async function checkIfActivated(checkIfLogin){
                         let referBonus = ''
                         let UserEarning = ''
 
-                        const Ru2 = `<p class='earning1' style='font-size:medium' >реф вознаграждение: ${referBonus}</p>
-                    <p class='earning2'style='font-size:medium' >Доход фермы: ${UserEarning}</p>`
+                        const Ru2 = `<p class='earning1' style='font-size:medium' >Реф вознаграждение: ${referBonus}</p>
+                    <p class='earning2'style='font-size:medium' >Вознаграждения: ${UserEarning}</p>`
                         const Ru3 = `Активированная ферма`
 
                         const En2 = `<p class='earning1' style='font-size:medium' >Ref. reward: ${referBonus}</p>
